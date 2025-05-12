1553b246bcf297cf5cbf4c594a2a4df1a88c2dbd
Amin Tazifor <amin.tazifor@gmail.com>
2025-05-12 21:18:23 +0100

    Add TTL handling to IdempotencyStore with expiration logic
    Introduce a time-to-live (TTL) mechanism to automatically remove expired entries from the IdempotencyStore. This includes adding a timestamp to stored responses, checking for expiration during access, and logging expired keys when removed.

--- src/main/java/com/example/idempotency/store/IdempotencyStore.java
+++ src/main/java/com/example/idempotency/store/IdempotencyStore.java
@@ -1,24 +1,50 @@
 package com.example.idempotency.store;
 
 import com.example.idempotency.model.TopUpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class IdempotencyStore {
-    private final Map<String, TopUpResponse> processedRequests = new ConcurrentHashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(IdempotencyStore.class);
+    public static class Entry {
+        TopUpResponse response;
+        Instant timestamp;
+
+        public Entry(TopUpResponse response) {
+            this.response = response;
+            this.timestamp = Instant.now();
+        }
+    }
+
+    private final Map<String, Entry> processedRequests = new ConcurrentHashMap<>();
+    private final Duration ttl = Duration.ofSeconds(20);
 
     public TopUpResponse getTopUpResponse(String key) {
-        return processedRequests.get(key);
+        removeIfExpired(key);
+        return processedRequests.getOrDefault(key, new Entry(null)).response;
     }
 
     public boolean contains(String key) {
+        removeIfExpired(key);
         return processedRequests.containsKey(key);
     }
 
     public void store(String key, TopUpResponse topUpResponse) {
-        processedRequests.put(key, topUpResponse);
+        processedRequests.put(key, new Entry(topUpResponse));
+    }
+
+    public void removeIfExpired(String key) {
+        Entry entry = processedRequests.get(key);
+        if(entry != null && entry.timestamp.plus(ttl).isBefore(Instant.now())) {
+            logger.info("[EXPIRED] key={}", key);
+            processedRequests.remove(key);
+        }
     }
 }
